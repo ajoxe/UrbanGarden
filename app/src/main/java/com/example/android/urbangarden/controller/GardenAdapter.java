33c561e0306dc2326b39f4376243fdc37cb92736
C4Q <annekochanski@ac.c4q.nyc>
2018-03-03 16:00:26 -0500

    added adapter

--- app/src/main/java/com/example/android/urbangarden/controller/GardenAdapter.java
+++ app/src/main/java/com/example/android/urbangarden/controller/GardenAdapter.java
@@ -1,8 +1,43 @@
 package com.example.android.urbangarden.controller;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.urbangarden.GardenViewHolder;
+import com.example.android.urbangarden.R;
+import com.example.android.urbangarden.model.GardenData;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by amirahoxendine on 3/3/18.
  */
 
-public class GardenAdapter {
+public class GardenAdapter extends RecyclerView.Adapter<GardenViewHolder> {
+
+    List<GardenData> gardenDataList = new ArrayList<>();
+
+    public GardenAdapter(List<GardenData> gardenDataList) {
+        this.gardenDataList = gardenDataList;
+    }
+
+    @Override
+    public GardenViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.garden_item_view, parent, false);
+        return new GardenViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(GardenViewHolder holder, int position) {
+        holder.bind(gardenDataList.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return gardenDataList.size();
+    }
 }
