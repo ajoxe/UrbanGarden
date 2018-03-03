722ad7800d7c8f85e14a900fb9245db8408ae430
slinny <siranli411@gmail.com>
2018-03-03 13:36:37 -0500

    added options menu to get quick aceess to my fav list

--- app/src/main/java/com/example/android/urbangarden/GardenSearchActivity.java
+++ app/src/main/java/com/example/android/urbangarden/GardenSearchActivity.java
@@ -1,13 +1,42 @@
 package com.example.android.urbangarden;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class GardenSearchActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garden_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.garden_options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.fav:
+                Intent intent = new Intent(GardenSearchActivity.this, MyGardensActivity.class);
+//                intent.putExtra("myGardenList", "");
+                startActivity(intent);
+                Log.e(TAG,"Fav button was clicked");
+                break;
+            default:
+                Log.e(TAG,"No button was clicked");
+        }
+        return true;
     }
 }
