d43005c22b59f744245464ec467d10ddaeabf4b2
ajoxe <32021190+ajoxe@users.noreply.github.com>
2018-03-04 23:34:04 -0500

    Revert "Anne branch"

--- app/src/main/java/com/example/android/urbangarden/GardenDetailActivity.java
+++ app/src/main/java/com/example/android/urbangarden/GardenDetailActivity.java
@@ -6,9 +6,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -16,17 +13,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.android.urbangarden.database.GardensDataManager;
-import com.example.android.urbangarden.database.GardensDatabase;
 import com.example.android.urbangarden.location.MapsActivity;
-import com.example.android.urbangarden.userloginandregister.LoginActivity;
-import com.example.android.urbangarden.userloginandregister.MyPostsActivity;
-import com.example.android.urbangarden.userloginandregister.SettingsActivity;
-import com.example.android.urbangarden.userloginandregister.UserActivity;
 
 public class GardenDetailActivity extends AppCompatActivity {
-
-    public static final String TAG = GardenDetailActivity.class.getSimpleName();
 
     private TextView parkName, parkAddress;
     private ImageView parkImage;
@@ -35,8 +24,6 @@ public class GardenDetailActivity extends AppCompatActivity {
 
     private String name, address;
     Button getGardenLocation;
-    private Menu menu;
-    String user = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,8 +50,8 @@ public class GardenDetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(GardenDetailActivity.this, MapsActivity.class);
-                intent.putExtra("GardenName", parkName.getText().toString());
-                intent.putExtra("GardenAddress", parkAddress.getText().toString() + "New York, NY");
+                intent.putExtra("GardenName",parkName.getText().toString());
+                intent.putExtra("GardenAddress",parkAddress.getText().toString() + "New York, NY");
                 startActivity(intent);
             }
         });
@@ -88,99 +75,11 @@ public class GardenDetailActivity extends AppCompatActivity {
 
                 favesButton.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_filter_vintage_black2_24dp));
                 Toast.makeText(getApplicationContext(), getName + " added to favorites!", Toast.LENGTH_LONG).show();
-
             }
         });
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        this.menu = menu;
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.garden_options_menu, menu);
-        updateMenuTitles();
-        hideOption(user);
-        showOption(user);
-        return true;
-    }
-
-    private void hideOption(String user) {
-        MenuItem item = menu.findItem(R.id.login);
-        if (user != null) {
-            item.setVisible(false);
-        }
-    }
-
-    private void showOption(String user) {
-        MenuItem item = menu.findItem(R.id.login);
-        if (user == null) {
-            item.setVisible(true);
-        }
-    }
-
-    private void updateMenuTitles() {
-        MenuItem userMenuItem = menu.findItem(R.id.user);
-        if (user != null) {
-            userMenuItem.setTitle(user);
-        } else {
-            userMenuItem.setTitle("User");
-        }
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.login:
-                Intent intent = new Intent(GardenDetailActivity.this, LoginActivity.class);
-//                intent.putExtra("myGardenList", "");
-                startActivity(intent);
-                Log.e(TAG, "login button was clicked");
-                break;
-
-            case R.id.user_profile:
-                Intent intent0 = new Intent(GardenDetailActivity.this, UserActivity.class);
-//                intent.putExtra("myGardenList", "");
-                startActivity(intent0);
-                Log.e(TAG, "my posts button was clicked");
-                break;
-
-            case R.id.my_posts:
-                Intent intent1 = new Intent(GardenDetailActivity.this, MyPostsActivity.class);
-//                intent.putExtra("myGardenList", "");
-                startActivity(intent1);
-                Log.e(TAG, "my posts button was clicked");
-                break;
-
-            case R.id.fav_list:
-                GardensDataManager.getSavedGardens(GardensDatabase.getGardensDatabase(getApplicationContext()));
-                Intent intent2 = new Intent(GardenDetailActivity.this, MyGardensActivity.class);
-//                intent.putExtra("myGardenList", "");
-                startActivity(intent2);
-                Log.e(TAG, "Fav list button was clicked");
-                break;
-
-            case R.id.setting:
-                Intent intent3 = new Intent(GardenDetailActivity.this, SettingsActivity.class);
-//                intent.putExtra("myGardenList", "");
-                startActivity(intent3);
-                Log.e(TAG, "Setting was clicked");
-                break;
-
-            case R.id.sign_out:
-                user = null;
-                updateMenuTitles();
-                hideOption(user);
-                showOption(user);
-                Log.e(TAG, "Sign out button was clicked");
-                break;
-
-            default:
-                Log.e(TAG, "No button was clicked");
-        }
-        return true;
-    }
 
 
 }
