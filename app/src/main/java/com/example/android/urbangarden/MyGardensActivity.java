770ffa7c96feaa3cc674056016c08bb72bb7337a
C4Q <annekochanski@ac.c4q.nyc>
2018-03-04 23:33:16 -0500

    added menu to gardens activity

--- app/src/main/java/com/example/android/urbangarden/MyGardensActivity.java
+++ app/src/main/java/com/example/android/urbangarden/MyGardensActivity.java
@@ -7,21 +7,33 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.android.urbangarden.R;
 import com.example.android.urbangarden.controller.GardenAdapter;
 import com.example.android.urbangarden.database.GardensDataManager;
 import com.example.android.urbangarden.database.GardensDatabase;
 import com.example.android.urbangarden.model.Garden;
+import com.example.android.urbangarden.userloginandregister.LoginActivity;
+import com.example.android.urbangarden.userloginandregister.MyPostsActivity;
+import com.example.android.urbangarden.userloginandregister.SettingsActivity;
+import com.example.android.urbangarden.userloginandregister.UserActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyGardensActivity extends AppCompatActivity {
+
+    public static final String TAG = MyGardensActivity.class.getSimpleName();
+
     List<Garden> savedGardens = GardensDataManager.savedGardenList;
     private RecyclerView recyclerView;
     GardenAdapter gardenAdapter;
     Context context;
+    String user = null;
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,4 +53,93 @@ public class MyGardensActivity extends AppCompatActivity {
         recyclerView.setAdapter(gardenAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.garden_options_menu, menu);
+        updateMenuTitles();
+        hideOption(user);
+        showOption(user);
+        return true;
+    }
+
+    private void hideOption(String user) {
+        MenuItem item = menu.findItem(R.id.login);
+        if (user != null) {
+            item.setVisible(false);
+        }
+    }
+
+    private void showOption(String user) {
+        MenuItem item = menu.findItem(R.id.login);
+        if (user == null) {
+            item.setVisible(true);
+        }
+    }
+
+    private void updateMenuTitles() {
+        MenuItem userMenuItem = menu.findItem(R.id.user);
+        if (user != null) {
+            userMenuItem.setTitle(user);
+        } else {
+            userMenuItem.setTitle("User");
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.login:
+                Intent intent = new Intent(MyGardensActivity.this, LoginActivity.class);
+//                intent.putExtra("myGardenList", "");
+                startActivity(intent);
+                Log.e(TAG, "login button was clicked");
+                break;
+
+            case R.id.user_profile:
+                Intent intent0 = new Intent(MyGardensActivity.this, UserActivity.class);
+//                intent.putExtra("myGardenList", "");
+                startActivity(intent0);
+                Log.e(TAG, "my posts button was clicked");
+                break;
+
+            case R.id.my_posts:
+                Intent intent1 = new Intent(MyGardensActivity.this, MyPostsActivity.class);
+//                intent.putExtra("myGardenList", "");
+                startActivity(intent1);
+                Log.e(TAG, "my posts button was clicked");
+                break;
+
+            case R.id.fav_list:
+                GardensDataManager.getSavedGardens(GardensDatabase.getGardensDatabase(getApplicationContext()));
+                Intent intent2 = new Intent(MyGardensActivity.this, MyGardensActivity.class);
+//                intent.putExtra("myGardenList", "");
+                startActivity(intent2);
+                Log.e(TAG, "Fav list button was clicked");
+                break;
+
+            case R.id.setting:
+                Intent intent3 = new Intent(MyGardensActivity.this, SettingsActivity.class);
+//                intent.putExtra("myGardenList", "");
+                startActivity(intent3);
+                Log.e(TAG, "Setting was clicked");
+                break;
+
+            case R.id.sign_out:
+                user = null;
+                updateMenuTitles();
+                hideOption(user);
+                showOption(user);
+                Log.e(TAG, "Sign out button was clicked");
+                break;
+
+            default:
+                Log.e(TAG, "No button was clicked");
+        }
+        return true;
+    }
+
 }
